7f9ab107164d0cfc76588915590505a01951c448
Ghadeer <ghaora@gmail.com>
2021-12-02 14:19:56 +0200

    the result without colors or effects

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+let userInputName= prompt ('Inserisci nome');
+
+let userInputCognome= prompt ('Inserisci cognome');
+
+let userInputColore= prompt ('Inserisci il suo colore preferito');
+
+document.getElementById('myProfile').innerHTML= userInputName+ userInputCognome+userInputColore+"21";
+
